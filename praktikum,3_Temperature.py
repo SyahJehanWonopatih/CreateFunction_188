2af0dd211cd0dc82dcf59c2596bb27f636256ba3
SyahJehanWonopatih <syahjehan00@gmail.com>
2025-10-15 15:18:06 +0700

    first commit

---
+++ praktikum,3_Temperature.py
@@ -0,0 +1,23 @@
+#No 1
+def convert_temperature(value, unit):
+    if unit.upper() == 'C':
+        return(value * 9/5) + 32
+    elif unit.upper() == 'F':
+        return (value - 32) * 5/9
+    else:
+        raise ValueError("Unit must be 'C' or 'F'")
+    
+print("======== convert temperature ========" )
+try: 
+        input_temperature = float(input("enter the temperature value: "))
+        unit = input("Enter temperature units ('C' for celcius and 'F' for Fahrenhit): ")
+        convert = convert_temperature(input_temperature, unit)
+        if unit.upper() == 'C':
+            print(f"{input_temperature}C = {convert: 2f} F")
+        elif unit.upper() == 'F':
+            print(f"{input_temperature} F = {convert:.2f} C")
+        else:
+            print("invalid option.")
+except ValueError as e:
+        print("error", e)
+
---
+++ praktikum3_Circle.py
@@ -0,0 +1,15 @@
+#no
+#No 2
+import math
+
+# mendefinisi Lambda function untuk menghitung luas jari-jari lingkaran = π * r^2
+circle_area = lambda r: math.pi * r**2
+
+# Get radius from user input
+print(" Konversi Lingkaran ")
+r = float(input("Masukkan Jari-Jari Lingkaran: "))
+
+# menghitung luas
+area = circle_area(r)
+
+print(f"Luas Lingkaran {r} adalah: {area}")
